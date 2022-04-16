3246d79c204469db1fe9631af2a717bf9c56df6b
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-16 13:22:24 +0600

    signInWithGoogle auth:element return-issue-fixed

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Getting Started with Create React App
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-# what are I doing this project
+# what are I doing this project step by step
 * Created many component in Home components 
 * Routes install & used
 page href change to as Link to component 
@@ -13,10 +13,14 @@ page href change to as Link to component
 * navbar: fix reload page [ href="" to Link to=""]
 * useParameter for dynamically navigate 
 * jump same page using by "id"
+# Authentication implement
 * created login & signup form 
 * useRef & signup/login toggle
-* firebase Auth , require Auth & (redirect the site what 
+* firebase 
+ * create account with email-pass | login/logout Auth , require Auth & (redirect the site what 
 * visitor wanted/desire to go to visit) 
+* signing with social media
+* signInWithGoogle auth,navigate done: element return issue fixed 
 
 
 
--- src/Pages/Login/SocialMediaLogin/SocialMediaLogin.js
+++ src/Pages/Login/SocialMediaLogin/SocialMediaLogin.js
@@ -2,29 +2,62 @@ import React from 'react';
 import google from '../../../Images/social/google.png';
 import fb from '../../../Images/social/fb.jpg';
 import github from '../../../Images/social/github.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 const SocialMediaLogin = () => {
+    // google-1st
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    // g-c 
+    const navigate = useNavigate();
+    // social btn blink issue 
+    let errorBtnElement;
+    //g-a
+    // return: errorBtnElement=
+    if (error) {
+        errorBtnElement = <div>
+                <p className='text-danger text-center'>Error: {error.message}</p>
+            </div>
+    }
+
+    // if (loading) {
+    //     return <p>Loading...</p>;
+    // }
+
+    //g-b
+    if (user) {
+        // 1st-d
+        navigate ('/home');
+        // return (
+        //     <div>
+        //         <p>Signed In User: {user.email}</p>
+        //     </div>
+        // );
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center w-50 mx-auto'>
-                <div style={{height:'1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
                 <p className='mt-2 px-2'>Or</p>
-                <div style={{height:'1px'}} className='bg-primary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
             </div>
-                 {/* google signin btn  */}
-            <button className='btn btn-info w-50 d-block mx-auto my-2'>
-                <img style={{width:'30px'}} src={google} alt=""/>
+              {errorBtnElement} 
+            {/* google signin btn  */}
+            <button onClick={()=>signInWithGoogle()} className='btn btn-info w-50 d-block mx-auto my-2'>
+                <img style={{ width: '30px' }} src={google} alt="" />
                 <span className='px-2'>Sign In with Google</span>
-                </button>
-                {/* fb signin btn  */}
+            </button>
+            {/* fb signin btn  */}
             <button className='btn btn-primary w-50 d-block mx-auto my-2'>
-                <img style={{width:'36px', marginLeft:'6px'}} src={fb} alt=""/>
+                <img style={{ width: '36px', marginLeft: '6px' }} src={fb} alt="" />
                 <span className='px-2'>Sign In with Facebook</span>
-                </button>
-                {/* github signin btn  */}
+            </button>
+            {/* github signin btn  */}
             <button className='btn btn-dark w-50 d-block mx-auto my-2'>
-                <img style={{width:'30px'}} src={github} alt=""/>
+                <img style={{ width: '30px' }} src={github} alt="" />
                 <span className='px-2'>Sign In with Github</span>
-                </button>
+            </button>
         </div>
     );
 };
