ed6728f5ae8bdca2f4cefc23faffe9b469160e22
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-28 16:12:18 +0600

    import useAuthState and dynamically set email in url

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,16 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState }  from 'react';
-    // get data from db
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+
 const Order = () => {
-    const [orders, setOrders] = useState();
+    // userAuthTake 
+    const [user] = useAuthState(auth);
+    // show individual user data from db, based on email 
+    const [orders, setOrders] = useState([]);
     useEffect(()=>{
         const getOrderDataFromDb = async()=>{
-            const url = `http://localhost:5000/order`;
+            const email = user?.email; 
+            const url = `http://localhost:5000/order?email=${email}`;
             const {data} = await axios.get(url);
             setOrders(data);
         }
         getOrderDataFromDb();
-    },[])
+    },[user])
     return (
         <div>
             <h1>Your Orders: {orders.length}</h1>
